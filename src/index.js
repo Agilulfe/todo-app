91317df6e7beaa19a43869286e0286fa11305d28
Agilulfe <guillaume.jacobs1995@outlook.com>
2020-06-16 15:16:07 +0200

    packages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import { renderTodos } from './views'
 import { setFilters } from './filters'
 import { createTodo, loadTodos } from './todos'
